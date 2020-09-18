5627f51ebc32a53302a4fba296badea7ac904b60
sali73 <sally.elgendy@hotmail.com>
2020-09-18 18:33:19 -0400

    add buttons func and run game

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //jQuery Variables
 const $button = $(`.button`);
 const $userStringBox = $(`.guess-container`);
@@ -44,7 +43,6 @@ const $closeBtn4 = $('#close4');
 const $modalText4 = $('#modal-textbox4');
 const $modalWinHeader = $(`.win-modal-header`);
 
-
 ///////////////////
 //===Functions===//
 ///////////////////
@@ -77,7 +75,6 @@ const resetGame = () => {
     runGame();
 }
 
-
 //sets a new ronud
 const resetRound = () => {
     $userStringBox.empty();
@@ -87,7 +84,6 @@ const resetRound = () => {
     randomWord = ``;
     resetButtons();
     runGame();
-
 };
 
 //checks player score to see if anyone won
@@ -105,7 +101,6 @@ const checkScore = () => {
         openModal4();
         resetGame();
     }
-
 };
 
 //flips the player 
@@ -147,9 +142,7 @@ const checkGameStatus = () => {
         resetRound();
         flipPlayer();
         checkScore();
-
     } 
-
     if (guessesRemaining <= 0){
         openModal3();
         resetRound();
@@ -157,3 +150,152 @@ const checkGameStatus = () => {
         checkScore();
     }
 };
+
+//update the counters
+const updateCounters = () => {
+    $counters.empty();
+    $counters.append(`<h3 class = "guesses-remaining">Guesses Remaining: ${guessesRemaining}</h3><h3 class = "letters-remaining">Letters Remaining: ${lettersRemaining}</h3>`);
+};
+
+//actions when guess is right
+const guessIsCorrect = (target) => {
+    userWord[x] = splitRandomWord[x];
+    $userStringBox.empty();
+    $userStringBox.append(userWord);
+    target.off(`click`);
+    changeGreen(target);
+    updateCounters();
+    updateImage();
+    setTimeout(checkGameStatus, 6000);
+};
+
+//actions when guess is wrong
+const guessIsIncorrect = (target) => {
+    target.off(`click`);
+    changeRed(target);
+    updateCounters();
+    updateImage();
+    setTimeout(checkGameStatus, 6000);
+};
+
+//check the letter that is guessed
+const checkLetter = (letter, target) => {
+    const letterCheck = splitRandomWord.some((value, index) => {
+        return value == letter;
+    })
+    for (x = 0; x < splitRandomWord.length; x++){
+        if(letter == splitRandomWord[x]){
+            lettersRemaining--;
+            guessIsCorrect(target);
+        } 
+    }
+    if (letterCheck === false) {
+        guessesRemaining--;
+        guessIsIncorrect(target);
+    }
+};
+
+//get the random word
+const getRandomWord = () => {
+    randomWord = words[Math.floor(Math.random() * words.length)];
+};
+
+//set the user word to blank
+const setUserWord = () => {
+    splitRandomWord = randomWord.split(``);
+    for (x = 0; x < splitRandomWord.length; x++) {
+        userWord[x] = `_`;
+    }
+    $userStringBox.append(userWord);
+};
+
+//set the Counters
+const setCounters = () => {
+    lettersRemaining = splitRandomWord.length;
+    $counters.append(`<h3 class = "guesses-remaining">Guesses Remaining: ${guessesRemaining}</h3><h3 class = "letters-remaining">Letters Remaining: ${lettersRemaining}</h3>`);
+};
+
+//sets player scores
+const setPlayerScore = () => {
+    $playerOne.empty();
+    $playerTwo.empty();
+    $playerOne.append(`<h2>Player 1 Score: ${playerOneScore}</h4>`);
+    $playerTwo.append(`<h2>Player 2 Score: ${playerTwoScore}</h4>`);
+};
+
+//event listeners/handlers for lose round modal
+const openModal4 = () => {
+    $modal4.css('display', 'block');
+  };
+const closeModal4 = () => {
+    $modal4.css('display', 'none');
+  };
+$closeBtn4.on('click', closeModal4);
+
+//event listeners/handlers for lose round modal
+const openModal3 = () => {
+    $modal3.css('display', 'block');
+  };
+const closeModal3 = () => {
+    $modal3.css('display', 'none');
+  };
+$closeBtn3.on('click', closeModal3);
+
+//event listeners/handlers for win round modal
+const openModal2 = () => {
+    $modal2.css('display', 'block');
+  };  
+const closeModal2 = () => {
+    $modal2.css('display', 'none');
+  };
+$closeBtn2.on('click', closeModal2);
+
+//event listeners/handlers for instructions modal
+const openModal = () => {
+    $modal.css('display', 'block');
+    $openBtn.css(`box-shadow`, `0 0 3px 3px inset`);
+    $openBtn.css(`background-color`, `rgb(228, 228, 228)`);
+    $openBtn.css(`font-size`, `1.9rem`);
+  };
+  
+const closeModal = () => {
+    $modal.css('display', 'none');
+    $openBtn.css(`box-shadow`, `0 0 2px 2px`);
+    $openBtn.css(`background-color`, `white`);
+    $openBtn.css(`font-size`, `2rem`);
+  };
+
+$openBtn.on('click', openModal);
+$closeBtn.on('click', closeModal);
+
+//function to reset the buttons once the round is over
+const resetButtons = () => {
+    $button.off(`click`);
+    $button.css(`background-color`, `white`);
+    $button.css(`box-shadow`, "0 0 4px 4px");
+    $button.css(`font-size`, "1.5rem");
+    $button.on(`click`, (event) => {
+        const $element = $(event.currentTarget);
+        checkLetter($element.text().trim(), $element);
+    });
+
+}
+
+//Set event listener on buttons and animate them
+$button.on(`click`, (event) => {
+    const $element = $(event.currentTarget);
+    checkLetter($element.text().trim(), $element);
+    
+});
+
+
+//Main function for running the game
+runGame = () => {
+    $hangman.css(`background-image`, `url(img/first.png)`)
+    setPlayerScore();
+    getRandomWord();
+    setUserWord();
+    setCounters();
+}
+
+runGame();
